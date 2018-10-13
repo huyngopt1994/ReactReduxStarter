1eeee591d35f7d6ff4b117269e67b69b1aa16aea
vuhuy@inspectorio.com <huyngopt1994@gmail.com>
2018-10-14 00:37:43 +0700

    add state for messages with 2 persons

---
+++ skypey/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+// Component Sidebar
+import React from "react";
+import "./Sidebar.css";
+
+export const Sidebar = () => {
+    return <aside className="Sidebar">Sidebar</aside>
+};
+
+
---
+++ skypey/src/store/index.js
@@ -0,0 +1,54 @@
+const state = {
+    user: {
+        name: "Huy Ngo",
+        email: "huyngoabca@gmail.com",
+        profile_pic: "https://us.123rf.com/450wm/mozgostudia/mozgostudia1601/mozgostudia160101463/51384062-cartoon-little-devil-or-imp-vector-illustration.jpg?ver=6",
+        status: "learner",
+        id: '1sasasa'
+    },
+    messages: {
+        x_y: {
+            0: {
+                number: 0,
+                text: 'hello man!',
+                is_user_msg: false
+            },
+            1: {
+                number: 1,
+                text: 'Donging great. you?',
+                is_user_msg: true
+            }
+        },
+        x_y_1: {
+            0: {
+                number: 0,
+                text: 'you know redux!',
+                is_user_msg: false
+            },
+            1: {
+                number: 1,
+                text: 'I do. any gig?',
+                is_user_msg: true
+            }
+        }
+    },
+    typing: "",
+    activateUserId: 'x_y_1',
+    contacts: {
+        x_y_1: {
+            name: "Huy Ngo1",
+            email: "huyngoabca1@gmail.com",
+            profile_pic: "https://us.123rf.com/450wm/mozgostudia/mozgostudia1601/mozgostudia160101463/51384062-cartoon-little-devil-or-imp-vector-illustration.jpg?ver=6",
+            status: "learner 1",
+            id: '1sasasa_1'
+        },
+        x_y: {
+            name: "Huy Ngo2",
+            email: "huyngoabca2@gmail.com",
+            profile_pic: "https://us.123rf.com/450wm/mozgostudia/mozgostudia1601/mozgostudia160101463/51384062-cartoon-little-devil-or-imp-vector-illustration.jpg?ver=6",
+            status: "learner 2",
+            id: '1sasasa_2'
+        },
+
+    },
+};
